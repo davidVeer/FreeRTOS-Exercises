557045d7a95995c5f9af8f0c37752359721fbd74
davidVeer <davidvdveer05@gmail.com>
2025-06-19 21:42:08 +0200

    Getting Led working with led_strip.h Included
    also stopped tracking the managed_components

--- components/Assignment1Component/Assignment1Component.c
+++ components/Assignment1Component/Assignment1Component.c
@@ -6,25 +6,12 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_mac.h"
-
-
+#include "led_strip.h"
 
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
 static uint8_t s_led_state = 0;
 static const char *TAG = "LED";
-
-static void blink_led(void)
-{
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
 
 void run_Blink(void){
     configure_led();
@@ -36,4 +23,16 @@ void run_Blink(void){
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
+}
+
+static void blink_led(void)
+{
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
