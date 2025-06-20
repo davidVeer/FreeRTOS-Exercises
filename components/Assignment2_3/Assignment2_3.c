d8024db7845ab17f71807748593ccf4af7347a97
davidVeer <davidvdveer05@gmail.com>
2025-06-20 23:50:50 +0200

    Example 1 all Tasks are equal

--- components/Assignment2_3/Assignment2_3.c
+++ components/Assignment2_3/Assignment2_3.c
@@ -1,33 +1,58 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "esp_log.h"
 #include "Assignment2_3.h"
 
 static const char *TAG = "Assignment2_3";
 
 void run_Assignment2_3(){
-    
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task1",10000,1,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task2",10000,2,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task3",10000,3,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task4",10000,4,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task5",10000,5,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task6",10000,6,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task7",10000,7,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task8",10000,8,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task9",10000,9,0,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task10",10000,10,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task11",10000,11,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task12",10000,12,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task13",10000,13,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task14",10000,14,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task15",10000,15,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task16",10000,16,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task17",10000,17,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task18",10000,18,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task19",10000,19,1,NULL,0);
+    xTaskCreatePinnedToCore(
+        GenericTaskCode,"Task20",10000,20,1,NULL,0);
 }
 
 void GenericTaskCode(int taskNumber){
     for(;;){
         ESP_LOGI(TAG, "this is Task %d", taskNumber);
-    }
-}
-
-void DelayedTaskCode(int taskNumber){
-    for(;;){
-        ESP_LOGI(TAG, "this is Task %d", taskNumber);
-        vTaskDelay(10/portTICK_PERIOD_MS);
-    }
-}
-
-void WaitingTaskCode(int taskNumber){
-    for(;;){
-        ESP_LOGI(TAG, "this is Task %d", taskNumber);
-        
+        vTaskDelay(1);
     }
 }
 
--- main/main.c
+++ main/main.c
@@ -1,9 +1,9 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
-#include "Assignment2_2.h"
+#include "Assignment2_3.h"
 
 
 void app_main(void)
 {
-    run_Assignment2_2();
+    run_Assignment2_3();
 }
