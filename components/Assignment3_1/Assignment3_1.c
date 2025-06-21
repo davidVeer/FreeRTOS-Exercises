3114bac7962ff51b90b67864a2583af3f557150e
davidVeer <davidvdveer05@gmail.com>
2025-06-21 11:53:07 +0200

    skeleton for 3.1

---
+++ components/Assignment3_1/Assignment3_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "Assignment3_1.h"
+
+void run_Assignment3_1(){
+    
+}
---
+++ components/Assignment3_1/include/Assignment3_1.h
@@ -0,0 +1,6 @@
+#ifndef ASSIGNMENT3_1_H
+#define ASSIGNMENT3_1_H
+
+void run_Assignment3_1();
+
+#endif
